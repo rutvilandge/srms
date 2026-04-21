7fba63c52e919fbed9c859ccfcee3980e9ccf0f3
Rutvi Landge <rutvilandge@gmail.com>
2026-04-21 23:59:52 +0530

    Update manage_students.php

--- php/manage_students.php
+++ php/manage_students.php
@@ -1,5 +1,6 @@
 <?php
-require_once 'config.php';
+session_start();
+require_once __DIR__ . '/config.php';
 if (!isset($_SESSION['admin_id'])) { header('Location: admin_login.php'); exit(); }
 
 $msg = '';
