a96626befd788ebd9aaddb76e0b4d77d26d9908c
Rutvi Landge <rutvilandge@gmail.com>
2026-04-21 12:12:31 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,12 +1,9 @@
 <?php
-$host     = 'shinkansen.proxy.rlwy.net';
-$username = 'root';
-$password = 'XfJQFQOEsPfqDqbWjcBzsTXNgXMIVjlj';
-$database = 'railway';
-$port     = 21818;
-
-$conn = mysqli_connect($host, $username, $password, $database, $port);
-
+define('DB_HOST', 'sql100.infinityfree.com'); 
+define('DB_USER', 'if0_41711243');       
+define('DB_PASS', 'WeobiB4bxvA');        
+define('DB_NAME', 'if0_41711243_srms_db');
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
